3a126c7db2bbf6c7cfc0498a3d23e0c65f02b2e3
Iain Earl <iain@itmecho.com>
2021-03-05 11:05:59 +0000

    nvim: update go ftplugin

--- nvim/.config/nvim/after/ftplugin/go.lua
+++ nvim/.config/nvim/after/ftplugin/go.lua
@@ -1,26 +1,21 @@
-vim.bo.textwidth = 100
-vim.wo.colorcolumn = "100"
+vim.bo.textwidth = 120
+vim.wo.colorcolumn = "120"
 vim.bo.expandtab = false
 vim.bo.tabstop = 4
 vim.bo.shiftwidth = 4
 vim.bo.softtabstop = 4
 
-function GoLines()
+function RunFormatCommand(cmd)
 	local view = vim.fn.winsaveview()
-	vim.cmd('silent execute "%!golines"')
+	vim.cmd('silent execute "%!' .. cmd .. '"')
 	if vim.v.shell_error ~= 0 then
 		vim.cmd('undo')
 	end
 	vim.fn.winrestview(view)
 end
 
-function GoFmt()
-	local view = vim.fn.winsaveview()
-	vim.cmd('silent execute "%!gofmt"')
-	if vim.v.shell_error ~= 0 then
-		vim.cmd('undo')
-	end
-	vim.fn.winrestview(view)
-end
+vim.cmd('command! -bang GoFmt call v:lua.RunFormatCommand("gofmt")')
+vim.cmd('command! -bang GoImports call v:lua.RunFormatCommand("goimports")')
+vim.cmd('command! -bang GoLines call v:lua.RunFormatCommand("golines -m 120 --base-formatter=gofmt")')
 
-vim.cmd('autocmd BufWritePre *.go call v:lua.GoFmt()')
+vim.cmd('autocmd BufWritePre *.go GoFmt')
