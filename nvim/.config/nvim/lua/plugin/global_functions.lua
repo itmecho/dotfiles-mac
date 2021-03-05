4688d844a1ead90eae3d761d1bc11f41b992509e
Iain Earl <iain@itmecho.com>
2021-03-05 22:46:36 +0000

    Consolidate random functions

--- nvim/.config/nvim/lua/plugin/global_functions.lua
+++
@@ -1,8 +0,0 @@
-function RunFormatCommand(cmd)
-    local view = vim.fn.winsaveview()
-    vim.cmd('silent execute "%!' .. cmd .. '"')
-    if vim.v.shell_error ~= 0 then
-        vim.cmd("undo")
-    end
-    vim.fn.winrestview(view)
-end
--- nvim/.config/nvim/lua/plugin/utils.lua
+++ nvim/.config/nvim/lua/plugin/utils.lua
@@ -3,3 +3,12 @@ function RemoveTrailingWhitespace()
     vim.cmd([[keeppatterns %s/\s\+$//e]])
     vim.fn.winrestview(view)
 end
+
+function RunFormatCommand(cmd)
+    local view = vim.fn.winsaveview()
+    vim.cmd('silent execute "%!' .. cmd .. '"')
+    if vim.v.shell_error ~= 0 then
+        vim.cmd("undo")
+    end
+    vim.fn.winrestview(view)
+end
